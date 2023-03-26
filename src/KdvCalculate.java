3ba00f46d9f85a34295fc980d9c01a3e5f1f7cce
iCanOzcan <icanozcan96@gmail.com>
2023-03-26 17:00:01 +0300

    KdvCalculate
    patika.dev icin kdv hesaplayan program

---
+++ src/KdvCalculate.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class KdvCalculate {
+
+    public static void main(String args[]){
+        Scanner input = new Scanner(System.in);
+        double amount,kdvRate=0.18;
+        System.out.print("Urun tutarini giriniz:");
+        amount = input.nextDouble();
+        if(0<amount && amount<1000)
+        {
+            double kdv = amount*kdvRate;
+            double result = amount+kdv;
+            System.out.println("Urunun kdv olmadan fiyati :"+amount+"tl"+" Urunun kdv tutari:"+kdv +"tl"+ " Urunun kdv dahil fiyati:"+ result+"tl");
+        }
+        else if (amount>=1000)
+        {
+            double kdv = amount*kdvRate;
+            double result = amount+kdv;
+            System.out.println("Urunun kdv olmadan fiyati :"+amount+"tl"+" Urunun kdv tutari:"+kdv +"tl"+ " Urunun kdv dahil fiyati:"+ result+"tl");
+        }
+        else{
+            System.out.println("Hatali veya eksik tutar girdiniz");
+        }
+
+
+
+    }
+}
